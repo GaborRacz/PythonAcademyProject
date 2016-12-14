a71c29ceefea554fae365a218dc6d3bafd95a615
Gabor Racz <blackspike182@gmail.com>
2016-12-14 10:09:35 +0100

    Added DocumentManager class: add, update, remove, list, load and search by title work

--- documents.py
+++ documents.py
@@ -1,8 +1,12 @@
+import os
+
+
 class Document(object):
     """Document of the repository"""
 
     def __init__(self, title, description, author, files, doc_format):
         self._title = title
+        self._spaceless_title = title.replace(" ", "")
         self._description = description
         self._author = author
         self._files = files
@@ -17,6 +21,14 @@ class Document(object):
     @title.setter
     def title(self, value):
         self._title = value
+
+    @property
+    def spaceless_title(self):
+        return self._spaceless_title
+
+    @spaceless_title.setter
+    def spaceless_title(self, value):
+        raise ValueError("You can't set the spaceless title.")
 
     @property
     def description(self):
@@ -80,15 +92,58 @@ class Document(object):
 class DocumentManager(object):
     """Manage documents"""
 
-    def __init__(self):
-        pass
+    def __init__(self, document_location):
+        self._document_location = document_location
 
     def add_document(self, document):
+        with open(os.path.join(self._document_location, document.spaceless_title), 'w') as doc_file:
+            doc_file.write(document.title + '\n')
+            doc_file.write(document.description +'\n')
+            doc_file.write(document.author + '\n')
+            doc_file.write(document.files + '\n')
+            doc_file.write(document.state + '\n')
+            doc_file.write(document.is_public() + '\n')
+
+    def update_document(self, document):
+        self.remove_document(document)
+        self.add_document(document)
+
+    def remove_document(self, document):
+        doc_file_path = os.path.join(self._document_location, document.spaceless_title)
+        if os.path.exists(doc_file_path):
+            os.remove(doc_file_path)
+        else:
+            raise ValueError('The document {} does not exist!'.format(document.spaceless_title))
+
+    def list_documents(self):
+        return [f for f in os.listdir(self._document_location) if os.path.isfile(os.path.join(self._document_location, f))]
+
+    def load_document(self, title):
+        with open(os.path.join(self._document_location, spaceless_title)) as doc_file:
+            title = doc_file.readline().strip()
+            author = doc_file.readline().strip()
+            files = doc_file.readline().strip()
+            doc_format = doc_file.readline().strip()
+            state = doc_file.readline().strip()
+            is_public = doc_file.readline().strip()
+        doc = Document(title, author, files, doc_format, state)
+
+        if is_public == "True":
+            doc.make_public()
+
+        return doc
+
+    def find_document_by_title(self, title):
+        spaceless_title = title.replace(" ", "")
+        doc_file_path = os.path.join(self._document_location, spaceless_title)
+        if os.path.exists(doc_file_path):
+            doc = self.load_document(spaceless_title)
+            return doc
+        else:
+            raise ValueError('The document {} does not exist!'.format(spaceless_title))
+
+    def find_document_by_author(self):
         pass
 
-    def update_document(self, document_id, document):
+    def find_document_by_format(self):
         pass
-
-    def remove_document(self, document_id):
-        pass
-
