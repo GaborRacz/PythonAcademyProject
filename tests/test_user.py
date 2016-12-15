051154ac7e946e5cb3d27aa339a7ddff5a8b5334
Gabor Racz <blackspike182@gmail.com>
2016-12-15 10:04:28 +0100

    Task 2. Added Unittests to TestUser to check creation, names and email addresses containing @ and domains.

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,26 @@
 import unittest
 
+from users import User
+from usergen import generator
 
 class TestUser(unittest.TestCase):
     """Test the user class"""
 
-    pass
+    def test_creation(self):
+        user_generator = generator.UserGenerator()
+        user = user_generator.generate_user()
+
+    def test_names(self):
+        user_generator = generator.UserGenerator()
+        user = user_generator.generate_user()
+        self.assertTrue(user.first_name == user.first_name.title())
+        self.assertTrue(user.family_name == user.family_name.title())
+
+    def test_email(self):
+        user_generator = generator.UserGenerator()
+        for i in xrange(100):
+            user = user_generator.generate_user()
+            self.assertTrue('@' in user.email)
+            self.assertTrue(any(domain in user.email for domain in generator.email_domains))
+
+
