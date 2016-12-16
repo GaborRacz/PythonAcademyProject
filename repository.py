40d63d5035cad35c392fa92457a857f29d9b1229
Gabor Racz <blackspike182@gmail.com>
2016-12-16 14:31:47 +0100

    Task 5. Added documentation to the Reposititory class

--- repository.py
+++ repository.py
@@ -9,11 +9,7 @@ The repository is in a dedicated directory. This directory contains the followin
     paths.ini   - the path of the main parts of the repository
     roles.txt   - user roles
 
-The documents directory contains subdirectories which name is the document identifier.
-
-For document metadata we save them to text files with the same name and .info extension next to the directories.
-
-The paths.ini file contains the (relative or absolute) paths of mentioned subdirectories.
+The documents directory contains files which identify the document.
 
 The roles.txt contains the user names and the list of assigned roles.
 """
@@ -61,6 +57,7 @@ class Repository(object):
         self._creation_date = datetime.now()
 
     def create_default_path_file(self):
+        """Creates the paths.ini file."""
         data = {
             'directories': {
                 'documents': 'documents',
@@ -72,32 +69,56 @@ class Repository(object):
         write_ini_file('{}/paths.ini'.format(self._location), data)
 
     def import_documents(self, path):
+        """Used to import many documents into the repository from the given path."""
         if self._user_manager.count_users() < 1:
             raise ValueError("Can't import without users")
-
         for document_id in os.listdir(path):
-            shutil.copy(os.path.join(path, document_id), os.path.join(self._document_manager._document_location, "documents"))
+            shutil.copy(os.path.join(path, document_id), self._document_manager._document_location)
 
     def export_documents(self, document_ids, target):
+        """
+        Exports the requested documents (by ID) to the given target location
+        :param document_ids: list of unique identifier of the requested documents
+        :param target: target directory where the files should be exported to
+        """
         for id in document_ids:
             for document in os.listdir(self._document_manager._document_location):
                 if id == document:
-                    shutil.copy(os.path.join(self._document_manager._document_location, "documents", id), target)
+                    shutil.copy(os.path.join(self._document_manager._document_location, id), target)
 
     def create_backup(self, name, target="/backup"):
+        """
+        Backup creation process
+        It contains all data and metadata of the repository.
+        Its aim is to reflect the state of the repository at the given time.
+        :param name: Name of the backup i.e.: 20161216
+        :param target: Target location of the backup
+        """
         self.backup_creation_date = datetime.now()
         self.users = self._user_manager.count_users()
         self.documents = self._document_manager.count_documents()
         shutil.copytree(self._location, os.path.join(target, name))
 
     def restore_backup(self, name, source="/backup"):
+        """
+        Restores the repository from the given backup to the repositories initial location
+        :param name: Name of the backup which should be restored
+        :param source: The location of the backup
+        """
         shutil.rmtree(self._location)
         shutil.copytree(os.path.join(source, name), self._location)
 
     def show_backup_info(self):
+        """
+        Prints out the information about the last backup.
+        """
         print("The backup was taken at {} .".format(self.backup_creation_date))
         print("There were {} users in the repository.".format(self.users))
         print("There were {} documents in the repository.".format(self.documents))
 
     def create_report(self):
+        """
+        Creates a report from the repository.
+        :return: Report type object
+        """
         return reports.Report()
