739b9645474e302617799d4bccdce6fa938a11c5
Gabor Racz <blackspike182@gmail.com>
2016-12-16 14:48:27 +0100

    Task 5. Added documentation to Review class, removed incompatible test cases

--- reviews.py
+++ reviews.py
@@ -1,4 +1,7 @@
 class Review(object):
+    """
+    Represents the process of evaluation for a document.
+    """
     def __init__(self):
         self.submission = False
         self.review_request1 = False
@@ -8,36 +11,61 @@ class Review(object):
         self.evaluation = False
 
     def set_submission(self, message):
+        """
+        Set submission flag only if all flags are False
+        :param message: message of the submission
+        """
         if self.submission is self.review_request1 is self.review_request2 is self.review_response1 is self.review_response2 is self.evaluation is False:
             self.submission = True
         else:
             raise ValueError(message)
 
     def set_review_request_1(self, message):
+        """
+        Set review_request_1 flag only if there has been a submission, and nothing else
+        :param message: message of the submission
+        """
         if self.submission is True and self.review_request1 is self.review_response1 is self.review_response2 is self.evaluation is False:
             self.review_request1 = True
         else:
             raise ValueError(message)
 
     def set_review_request_2(self, message):
+        """
+        Set review_request_2 flag only if there has been a submission, and nothing else
+        :param message: message of the submission
+        """
         if self.submission is True and self.review_request2 is self.review_response1 is self.review_response2 is self.evaluation is False:
             self.review_request2 = True
         else:
             raise ValueError(message)
 
     def set_review_response_1(self, message):
+        """
+        Set review_response_1 flag only if there has been a submission, and a request1
+        :param message: message of the submission
+        """
         if self.submission is self.review_request1 is True and self.review_response1 is self.evaluation is False:
             self.review_response1 = True
         else:
             raise ValueError(message)
 
     def set_review_response_2(self, message):
+        """
+        Set review_response_2 flag only if there has been a submission, and a request2
+        :param message: message of the submission
+        """
         if self.submission is self.review_request2 is True and self.review_response2 is self.evaluation is False:
             self.review_response2 = True
         else:
             raise ValueError(message)
 
     def set_evaluation_result(self, message):
+        """
+        Set evaluation_result flag only if there has been a submission,
+            request1, request2, response1 and response2, but no evaluation yet
+        :param message: message of the submission
+        """
         if self.evaluation is False and self.submission is self.review_request1 is self.review_request2 is self.review_response1 is self.review_response2 is True:
             self.evaluation = True
         else:
@@ -57,7 +85,7 @@ class ReviewManager(object):
         self.documents.append(submission_id)
 
     def submit_document(self, source, target, submission_id):
-        Review().set_submission()
+        Review().set_submission(submission_id)
 
     def save_review(self):
         pass
